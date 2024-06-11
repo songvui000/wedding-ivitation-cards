694176f2ced420c2c1173611faa4db4869a1f72e
Anh Viet <nguyen.anh.viet@moneyforward.vn>
2024-06-12 01:07:34 +0700

    update script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Script from 'next/script'
 import Wedding from '@/components/wedding/Wedding'
 import { useEffect, useState } from 'react'
 
@@ -8,6 +7,20 @@ function Home() {
 
   useEffect(() => {
     setIsMounted(true)
+
+    const script = document.createElement('script')
+    script.src = 'webfont.js'
+    document.body.appendChild(script)
+
+    const script1 = document.createElement('script')
+
+    script1.src = 'jquery.js'
+    document.body.appendChild(script1)
+
+    const script2 = document.createElement('script')
+
+    script2.src = 'webflow.js'
+    document.body.appendChild(script2)
   }, [])
 
   if (!isMounted) return null
@@ -15,9 +28,9 @@ function Home() {
   return (
     <>
       <Wedding />
-      <Script src="webfont.js" type="text/javascript" />
+      {/* <Script src="webfont.js" type="text/javascript" />
       <Script src="jquery.js" type="text/javascript" />
-      <Script src="webflow.js" type="text/javascript" strategy="afterInteractive" />
+      <Script src="webflow.js" type="text/javascript" strategy="afterInteractive" /> */}
     </>
   )
 }
